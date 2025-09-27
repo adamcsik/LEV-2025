0bba3d726f3e837c419ac5405a4a1533bc9153d1
adamcsik <111059992+adamcsik@users.noreply.github.com>
2025-09-27 13:03:30 +0200

    Update p023.py

--- p023.py
+++ p023.py
@@ -4,4 +4,27 @@ A színe legyen piros
 A rajzolás induljon a h betűre
 Kilépés legyen q betűre
 '''
+import turtle
 
+def inditas():
+    turtle.hideturtle()
+    turtle.clear()
+    turtle.pencolor("red")
+    turtle.pensize(5)
+    turtle.penup()
+    turtle.goto(-75, -37.5)
+    turtle.pendown()
+
+    for i in range(3):
+        turtle.forward(150)
+        turtle.left(120)
+
+
+#applikáció
+ablak = turtle.Screen()
+ablak.bgcolor("gray")
+
+turtle.onkey(inditas, "h")
+turtle.listen()
+turtle.onkey(turtle.bye, "q")
+turtle.mainloop()
